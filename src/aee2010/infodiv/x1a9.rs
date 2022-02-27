d12bb3bb87183ced6d014995f4b3a926b18b72ea
Thibaud Rouillé <thibaud.rouille@gmail.com>
2022-02-27 20:54:51 +0100

    Finished AEE 2010 infodiv x1a9 frame

--- src/aee2010/infodiv/x1a9.rs
+++ src/aee2010/infodiv/x1a9.rs
@@ -3,7 +3,6 @@ use core::fmt;
 use byteorder::{ByteOrder, NetworkEndian};
 
 use crate::{
-    mfd::{Menu, Popup, TripComputerPage, UserAction2010},
     vehicle::{AutomaticParkingMode, CruiseControlCustomSettingPosition},
     Error, Result,
 };
@@ -967,20 +966,84 @@ impl fmt::Display for Repr {
 mod test {
     use super::{Frame, Repr};
     use crate::{
-        mfd::{Menu, Popup, TripComputerPage, UserAction2010},
+        vehicle::{AutomaticParkingMode, CruiseControlCustomSettingPosition},
         Error,
     };
 
-    static REPR_FRAME_BYTES_1: [u8; 8] = [0x08, 0x00, 0x00, 0x00, 0x7f, 0xff, 0x00, 0x00];
-    static REPR_FRAME_BYTES_2: [u8; 8] = [0x08, 0x10, 0x00, 0x00, 0xff, 0xff, 0x05, 0xa8];
+    static REPR_FRAME_BYTES_1: [u8; 8] = [0x85, 0x00, 0x00, 0x55, 0x00, 0x14, 0x01, 0x50];
+    static REPR_FRAME_BYTES_2: [u8; 8] = [0x2a, 0x80, 0x21, 0xaa, 0x33, 0x69, 0xd0, 0xab];
 
     fn frame_1_repr() -> Repr {
         Repr {
+            trip_computer_secondary_trip_reset_request: true,
+            trip_computer_primary_trip_reset_request: false,
+            adaptive_cruise_control_button_state: true,
+            automatic_parking_mode: AutomaticParkingMode::SCP6,
+            telematics_enabled: false,
+            black_panel_enabled: true,
+            interactive_message: 0,
+            stop_check_request: false,
+            cruise_control_custom_speed_memorization_request: true,
+            available_space_measurement_button_state: false,
+            parking_sensors_button_state: true,
+            auto_main_beam_button_state: false,
+            lane_centering_button_state: true,
+            blind_spot_monitoring_button_state: false,
+            adaptive_cruise_control_plus_button_state: true,
+            adaptive_cruise_control_minus_button_state: false,
+            cruise_control_speed_instruction: 0,
+            indirect_under_inflation_button_state: false,
+            automatic_parking_state_change_request: true,
+            collision_alert_failure_display_request: false,
+            cruise_control_speed_setting_instruction_position:
+                CruiseControlCustomSettingPosition::Position1,
+            fault_check_request: true,
+            telematic_screen_lighting_level: 0,
+            telematic_unit_life_state: 0,
+            stop_start_button_state: false,
+            visual_parking_assistance_button_state: 0,
+            cruise_control_speed_instruction_value_request: false,
+            visual_parking_assistance_panoramic_view_button_state: true,
+            front_visual_parking_assistance_button_state: false,
+            rear_visual_parking_assistance_button_state: true,
+            visual_parking_assistance_activation_request: false,
         }
     }
 
     fn frame_2_repr() -> Repr {
         Repr {
+            trip_computer_secondary_trip_reset_request: false,
+            trip_computer_primary_trip_reset_request: true,
+            adaptive_cruise_control_button_state: false,
+            automatic_parking_mode: AutomaticParkingMode::SCP9,
+            telematics_enabled: true,
+            black_panel_enabled: false,
+            interactive_message: 33,
+            stop_check_request: true,
+            cruise_control_custom_speed_memorization_request: false,
+            available_space_measurement_button_state: true,
+            parking_sensors_button_state: false,
+            auto_main_beam_button_state: true,
+            lane_centering_button_state: false,
+            blind_spot_monitoring_button_state: true,
+            adaptive_cruise_control_plus_button_state: false,
+            adaptive_cruise_control_minus_button_state: true,
+            cruise_control_speed_instruction: 51,
+            indirect_under_inflation_button_state: true,
+            automatic_parking_state_change_request: false,
+            collision_alert_failure_display_request: true,
+            cruise_control_speed_setting_instruction_position:
+                CruiseControlCustomSettingPosition::Position6,
+            fault_check_request: false,
+            telematic_screen_lighting_level: 8,
+            telematic_unit_life_state: 2,
+            stop_start_button_state: true,
+            visual_parking_assistance_button_state: 3,
+            cruise_control_speed_instruction_value_request: true,
+            visual_parking_assistance_panoramic_view_button_state: false,
+            front_visual_parking_assistance_button_state: true,
+            rear_visual_parking_assistance_button_state: false,
+            visual_parking_assistance_activation_request: true,
         }
     }
 
@@ -988,48 +1051,86 @@ mod test {
     fn test_frame_1_deconstruction() {
         let frame = Frame::new_unchecked(&REPR_FRAME_BYTES_1);
         assert_eq!(frame.check_len(), Ok(()));
-        assert_eq!(frame.mfd_trip_computer_page(), TripComputerPage::Nothing);
-        assert_eq!(frame.maintenance_reset_request(), false);
-        assert_eq!(frame.emergency_call_in_progress(), false);
-        assert_eq!(frame.fault_recall_request(), false);
-        assert_eq!(frame.trip_computer_secondary_trip_reset_request(), false);
+        assert_eq!(frame.trip_computer_secondary_trip_reset_request(), true);
         assert_eq!(frame.trip_computer_primary_trip_reset_request(), false);
-        assert_eq!(frame.pre_conditioning_time(), 0);
+        assert_eq!(frame.adaptive_cruise_control_button_state(), true);
+        assert_eq!(frame.auto_parking_mode(), AutomaticParkingMode::SCP6);
         assert_eq!(frame.telematics_enabled(), false);
-        assert_eq!(frame.black_panel_enabled(), false);
-        assert_eq!(frame.indirect_under_inflation_reset_request(), false);
-        assert_eq!(frame.pre_conditioning_request(), false);
-        assert_eq!(frame.total_trip_distance(), 0);
-        assert_eq!(frame.interactive_message(), 32767);
+        assert_eq!(frame.black_panel_enabled(), true);
+        assert_eq!(frame.interactive_message(), 0);
         assert_eq!(frame.stop_check_request(), false);
-        assert_eq!(frame.popup_id_ack(), Popup::NoDisplay);
-        assert_eq!(frame.selected_menu(), Menu::WifiSettings);
-        assert_eq!(frame.wifi_parameters_ack(), false);
-        assert_eq!(frame.user_action_on_mfd(), UserAction2010::NoAction);
+        assert_eq!(frame.cruise_control_custom_speed_mem_request(), true);
+        assert_eq!(frame.available_space_measurement_button_state(), false);
+        assert_eq!(frame.parking_sensors_button_state(), true);
+        assert_eq!(frame.auto_main_beam_button_state(), false);
+        assert_eq!(frame.lane_centering_button_state(), true);
+        assert_eq!(frame.blind_spot_monitoring_button_state(), false);
+        assert_eq!(frame.adaptive_cruise_control_plus_button_state(), true);
+        assert_eq!(frame.adaptive_cruise_control_minus_button_state(), false);
+        assert_eq!(frame.cruise_control_speed_instruction(), 0);
+        assert_eq!(frame.indirect_under_inflation_button_state(), false);
+        assert_eq!(frame.auto_parking_state_change_request(), true);
+        assert_eq!(frame.collision_alert_failure_display_request(), false);
+        assert_eq!(
+            frame.cruise_control_spd_setting_instruction_pos(),
+            CruiseControlCustomSettingPosition::Position1
+        );
+        assert_eq!(frame.fault_check_request(), true);
+        assert_eq!(frame.telematic_screen_lighting_level(), 0);
+        assert_eq!(frame.telematic_unit_life_state(), 0);
+        assert_eq!(frame.stop_start_button_state(), false);
+        assert_eq!(frame.visual_parking_assistance_button_state(), 0);
+        assert_eq!(frame.cruise_control_spd_instruction_val_request(), false);
+        assert_eq!(
+            frame.visual_parking_assistance_panoramic_view_button_state(),
+            true
+        );
+        assert_eq!(frame.front_visual_parking_assistance_button_state(), false);
+        assert_eq!(frame.rear_visual_parking_assistance_button_state(), true);
+        assert_eq!(frame.visual_parking_assistance_activation_request(), false);
     }
 
     #[test]
     fn test_frame_2_deconstruction() {
         let frame = Frame::new_unchecked(&REPR_FRAME_BYTES_2);
         assert_eq!(frame.check_len(), Ok(()));
-        assert_eq!(frame.mfd_trip_computer_page(), TripComputerPage::Nothing);
-        assert_eq!(frame.maintenance_reset_request(), false);
-        assert_eq!(frame.emergency_call_in_progress(), false);
-        assert_eq!(frame.fault_recall_request(), false);
         assert_eq!(frame.trip_computer_secondary_trip_reset_request(), false);
-        assert_eq!(frame.trip_computer_primary_trip_reset_request(), false);
-        assert_eq!(frame.pre_conditioning_time(), 0);
+        assert_eq!(frame.trip_computer_primary_trip_reset_request(), true);
+        assert_eq!(frame.adaptive_cruise_control_button_state(), false);
+        assert_eq!(frame.auto_parking_mode(), AutomaticParkingMode::SCP9);
         assert_eq!(frame.telematics_enabled(), true);
         assert_eq!(frame.black_panel_enabled(), false);
-        assert_eq!(frame.indirect_under_inflation_reset_request(), false);
-        assert_eq!(frame.pre_conditioning_request(), false);
-        assert_eq!(frame.total_trip_distance(), 0);
-        assert_eq!(frame.interactive_message(), 32767);
+        assert_eq!(frame.interactive_message(), 33);
         assert_eq!(frame.stop_check_request(), true);
-        assert_eq!(frame.popup_id_ack(), Popup::ConnectedEmergencyCall);
-        assert_eq!(frame.selected_menu(), Menu::PrivacySettings);
-        assert_eq!(frame.wifi_parameters_ack(), true);
-        assert_eq!(frame.user_action_on_mfd(), UserAction2010::Yes);
+        assert_eq!(frame.cruise_control_custom_speed_mem_request(), false);
+        assert_eq!(frame.available_space_measurement_button_state(), true);
+        assert_eq!(frame.parking_sensors_button_state(), false);
+        assert_eq!(frame.auto_main_beam_button_state(), true);
+        assert_eq!(frame.lane_centering_button_state(), false);
+        assert_eq!(frame.blind_spot_monitoring_button_state(), true);
+        assert_eq!(frame.adaptive_cruise_control_plus_button_state(), false);
+        assert_eq!(frame.adaptive_cruise_control_minus_button_state(), true);
+        assert_eq!(frame.cruise_control_speed_instruction(), 51);
+        assert_eq!(frame.indirect_under_inflation_button_state(), true);
+        assert_eq!(frame.auto_parking_state_change_request(), false);
+        assert_eq!(frame.collision_alert_failure_display_request(), true);
+        assert_eq!(
+            frame.cruise_control_spd_setting_instruction_pos(),
+            CruiseControlCustomSettingPosition::Position6
+        );
+        assert_eq!(frame.fault_check_request(), false);
+        assert_eq!(frame.telematic_screen_lighting_level(), 8);
+        assert_eq!(frame.telematic_unit_life_state(), 2);
+        assert_eq!(frame.stop_start_button_state(), true);
+        assert_eq!(frame.visual_parking_assistance_button_state(), 3);
+        assert_eq!(frame.cruise_control_spd_instruction_val_request(), true);
+        assert_eq!(
+            frame.visual_parking_assistance_panoramic_view_button_state(),
+            false
+        );
+        assert_eq!(frame.front_visual_parking_assistance_button_state(), true);
+        assert_eq!(frame.rear_visual_parking_assistance_button_state(), false);
+        assert_eq!(frame.visual_parking_assistance_activation_request(), true);
     }
 
     #[test]
@@ -1037,24 +1138,39 @@ mod test {
         let mut bytes = [0x00; 8];
         let mut frame = Frame::new_unchecked(&mut bytes);
 
-        frame.set_mfd_trip_computer_page(TripComputerPage::Nothing);
-        frame.set_maintenance_reset_request(false);
-        frame.set_emergency_call_in_progress(false);
-        frame.set_fault_check_recall_request(false);
-        frame.set_trip_computer_secondary_trip_reset_request(false);
+        frame.set_trip_computer_secondary_trip_reset_request(true);
         frame.set_trip_computer_primary_trip_reset_request(false);
-        frame.set_pre_conditioning_time(0);
+        frame.set_adaptive_cruise_control_button_state(true);
+        frame.set_auto_parking_mode(AutomaticParkingMode::SCP6);
         frame.set_telematics_enabled(false);
-        frame.set_black_panel_enabled(false);
-        frame.set_indirect_under_inflation_reset_request(false);
-        frame.set_pre_conditioning_request(false);
-        frame.set_total_trip_distance(0);
-        frame.set_interactive_message(32767);
+        frame.set_black_panel_enabled(true);
+        frame.set_interactive_message(0);
         frame.set_stop_check_request(false);
-        frame.set_popup_id_ack(Popup::NoDisplay);
-        frame.set_selected_menu(Menu::WifiSettings);
-        frame.set_wifi_parameters_ack(false);
-        frame.set_user_action_on_mfd(UserAction2010::NoAction);
+        frame.set_cruise_control_custom_speed_mem_request(true);
+        frame.set_available_space_measurement_button_state(false);
+        frame.set_parking_sensors_button_state(true);
+        frame.set_auto_main_beam_button_state(false);
+        frame.set_lane_centering_button_state(true);
+        frame.set_blind_spot_monitoring_button_state(false);
+        frame.set_adaptive_cruise_control_plus_button_state(true);
+        frame.set_adaptive_cruise_control_minus_button_state(false);
+        frame.set_cruise_control_speed_instruction(0);
+        frame.set_indirect_under_inflation_button_state(false);
+        frame.set_auto_parking_state_change_request(true);
+        frame.set_collision_alert_failure_display_request(false);
+        frame.set_cruise_control_spd_setting_instruction_pos(
+            CruiseControlCustomSettingPosition::Position1,
+        );
+        frame.set_fault_check_request(true);
+        frame.set_telematic_screen_lighting_level(0);
+        frame.set_telematic_unit_life_state(0);
+        frame.set_stop_start_button_state(false);
+        frame.set_visual_parking_assistance_button_state(0);
+        frame.set_cruise_control_spd_instruction_val_request(false);
+        frame.set_visual_parking_assistance_panoramic_view_button_state(true);
+        frame.set_front_visual_parking_assistance_button_state(false);
+        frame.set_rear_visual_parking_assistance_button_state(true);
+        frame.set_visual_parking_assistance_activation_request(false);
 
         assert_eq!(frame.into_inner(), &REPR_FRAME_BYTES_1);
     }
@@ -1064,31 +1180,46 @@ mod test {
         let mut bytes = [0x00; 8];
         let mut frame = Frame::new_unchecked(&mut bytes);
 
-        frame.set_mfd_trip_computer_page(TripComputerPage::Nothing);
-        frame.set_maintenance_reset_request(false);
-        frame.set_emergency_call_in_progress(false);
-        frame.set_fault_check_recall_request(false);
         frame.set_trip_computer_secondary_trip_reset_request(false);
-        frame.set_trip_computer_primary_trip_reset_request(false);
-        frame.set_pre_conditioning_time(0);
+        frame.set_trip_computer_primary_trip_reset_request(true);
+        frame.set_adaptive_cruise_control_button_state(false);
+        frame.set_auto_parking_mode(AutomaticParkingMode::SCP9);
         frame.set_telematics_enabled(true);
         frame.set_black_panel_enabled(false);
-        frame.set_indirect_under_inflation_reset_request(false);
-        frame.set_pre_conditioning_request(false);
-        frame.set_total_trip_distance(0);
-        frame.set_interactive_message(32767);
+        frame.set_interactive_message(33);
         frame.set_stop_check_request(true);
-        frame.set_popup_id_ack(Popup::ConnectedEmergencyCall);
-        frame.set_selected_menu(Menu::PrivacySettings);
-        frame.set_wifi_parameters_ack(true);
-        frame.set_user_action_on_mfd(UserAction2010::Yes);
+        frame.set_cruise_control_custom_speed_mem_request(false);
+        frame.set_available_space_measurement_button_state(true);
+        frame.set_parking_sensors_button_state(false);
+        frame.set_auto_main_beam_button_state(true);
+        frame.set_lane_centering_button_state(false);
+        frame.set_blind_spot_monitoring_button_state(true);
+        frame.set_adaptive_cruise_control_plus_button_state(false);
+        frame.set_adaptive_cruise_control_minus_button_state(true);
+        frame.set_cruise_control_speed_instruction(51);
+        frame.set_indirect_under_inflation_button_state(true);
+        frame.set_auto_parking_state_change_request(false);
+        frame.set_collision_alert_failure_display_request(true);
+        frame.set_cruise_control_spd_setting_instruction_pos(
+            CruiseControlCustomSettingPosition::Position6,
+        );
+        frame.set_fault_check_request(false);
+        frame.set_telematic_screen_lighting_level(8);
+        frame.set_telematic_unit_life_state(2);
+        frame.set_stop_start_button_state(true);
+        frame.set_visual_parking_assistance_button_state(3);
+        frame.set_cruise_control_spd_instruction_val_request(true);
+        frame.set_visual_parking_assistance_panoramic_view_button_state(false);
+        frame.set_front_visual_parking_assistance_button_state(true);
+        frame.set_rear_visual_parking_assistance_button_state(false);
+        frame.set_visual_parking_assistance_activation_request(true);
 
         assert_eq!(frame.into_inner(), &REPR_FRAME_BYTES_2);
     }
 
     #[test]
     fn test_overlong() {
-        let bytes: [u8; 9] = [0x08, 0x00, 0x00, 0x00, 0x7f, 0xff, 0x00, 0x00, 0xff];
+        let bytes: [u8; 9] = [0x85, 0x00, 0x00, 0x55, 0x00, 0x14, 0x01, 0x50, 0xff];
         assert_eq!(
             Frame::new_unchecked(&bytes).check_len().unwrap_err(),
             Error::Overlong
@@ -1097,7 +1228,7 @@ mod test {
 
     #[test]
     fn test_underlong() {
-        let bytes: [u8; 7] = [0x08, 0x00, 0x00, 0x00, 0x7f, 0xff, 0x00];
+        let bytes: [u8; 7] = [0x85, 0x00, 0x00, 0x55, 0x00, 0x14, 0x01];
         assert_eq!(Frame::new_checked(&bytes).unwrap_err(), Error::Truncated);
     }
 
